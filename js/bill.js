22377fa581abcee49b0034d26a30610b6e19b8be
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-06-18 11:24:51 +0100

    sets up skeleton of form/inputs

---
+++ js/bill.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // const bill = document.querySelector('.bill');
+  const total = document.querySelector('.total');
+  const tip = document.querySelector('.tip');
+  const customers = document.querySelector('.customers');
+  const calculation = document.querySelector('.calculation');
+
+
+
+
+})
