bfe1a8e8c9ff27e2bb6f3185d0c2b92387a1e888
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-06-18 14:22:36 +0100

    calculates bill per payer and amends in response to change in bill amount

--- js/bill.js
+++ js/bill.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // const bill = document.querySelector('.bill');
-  const total = document.querySelector('.total');
-  const tip = document.querySelector('.tip');
-  const customers = document.querySelector('.customers');
-  const calculation = document.querySelector('.calculation');
+  // bill = document.querySelector('.bill');
+  totalDiv = document.querySelector('.total');
+  tipDiv = document.querySelector('.tip');
+  customersDiv = document.querySelector('.customers');
+  calculationDiv = document.querySelector('.calculation');
+  amountPerPayerP = document.querySelector('#amountPerPayer');
 
+  totalInput = document.querySelector('#totalInput');
 
+  total = 100;
+  tip = 0;
+  customers = 3;
+  calculation = 0;
+
+  calculateBillPerPayer();
+
+  totalInput.addEventListener('input', ()=> {
+    total = totalInput.value;
+    console.log(total);
+    calculateBillPerPayer();
+  })
+
+  function calculateBillPerPayer() {
+    calculation = ((total * (1 + tip/100)) / customers).toFixed(2);
+    amountPerPayerP.textContent = calculation;
+  }
 
 
 })
